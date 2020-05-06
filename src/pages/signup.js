78e75e623d74e90d3869d049ff24b03552f49a48
Bohdan12367 <58554650+Bohdan12367@users.noreply.github.com>
2020-05-07 01:53:53 +0300

    spatuuu

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,7 +4,7 @@ export class signup extends Component {
     render(){
         return (
             <div>
-                <h1> Sign up page</h1>
+                <h1>taras loh</h1>
             </div>
         )
     }

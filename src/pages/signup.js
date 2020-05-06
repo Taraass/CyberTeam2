f40a96862bcd56ed8f2ad9ff34206a28ff5c702c
Тарас <taras06031@gmail.com>
2020-05-06 23:22:13 +0300

    4.35

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,7 +4,7 @@ export class signup extends Component {
     render(){
         return (
             <div>
-                <h1> Signup page</h1>
+                <h1> Sign up page</h1>
             </div>
         )
     }

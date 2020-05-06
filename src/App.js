25e67fd0f4f41c03e19575280a2e557783a59517
Тарас <taras06031@gmail.com>
2020-05-06 21:51:27 +0300

    2.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-function App() {
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+class App extends Component {
+render(){
   return (
-    <div className="App">
-   <h1>Our file</h1>
-    </div>
+     <div className="App">
+         <Router>
+             <Switch>
+                 <Route exact path = "/" component={home}/>
+                    <Route exact path = "/login" component={login}/>
+                       <Route exact path = "/signup" component={signup}/>
+              </Switch>
+        </Router>
+      </div>
   );
+}
 }
 
 export default App;
